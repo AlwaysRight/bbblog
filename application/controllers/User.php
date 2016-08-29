11a4302a69d2c1af411db0fa968139d51561d81b
bb-PC\bb <798386353@qq.com>
2016-08-29 21:09:48 +0800

    model

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,7 +3,7 @@ class User extends CI_Controller{
     public function __construct()
     {
         parent::__construct();
-        $this->load->model("User_Model", "user_model");
+        $this->load->model("User_model", "user_model");
 
     }
 
